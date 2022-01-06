8f069a09a03f29cc1fec1467e8494d7da7999fcf
shockerli <shockerli@upfor.club>
2022-01-06 11:43:34 +0800

    support git log format time

--- time.go
+++ time.go
@@ -44,7 +44,6 @@ func TimeE(val interface{}) (t time.Time, err error) {
 func parseDate(s string) (t time.Time, err error) {
 	fs := []string{
 		time.RFC3339,
-		"2006-01-02T15:04:05", // iso8601 without timezone
 		time.RFC1123Z,
 		time.RFC1123,
 		time.RFC822Z,
@@ -53,6 +52,13 @@ func parseDate(s string) (t time.Time, err error) {
 		time.ANSIC,
 		time.UnixDate,
 		time.RubyDate,
+		time.Kitchen,
+		time.Stamp,
+		time.StampMilli,
+		time.StampMicro,
+		time.StampNano,
+		"2006-01-02T15:04:05",           // ISO8601 without timezone
+		"Mon Jan 2 15:04:05 2006 -0700", // Git log date
 		"2006-01-02 15:04:05.999999999 -0700 MST", // Time.String()
 		"2006-01-02",
 		"02 Jan 2006",
@@ -62,18 +68,25 @@ func parseDate(s string) (t time.Time, err error) {
 		"2006-01-02 15:04:05Z07:00", // RFC3339 without T
 		"2006-01-02 15:04:05Z0700",  // RFC3339 without T or timezone hh:mm colon
 		"2006-01-02 15:04:05",
+		"2006-01-02 15:04:05.000",
+		"2006-01-02 15:04:05.000000",
+		"2006-01-02 15:04:05.000000000",
 		"2006.01.02",
 		"2006.01.02 15:04:05",
+		"2006.01.02 15:04:05.000",
+		"2006.01.02 15:04:05.000000",
+		"2006.01.02 15:04:05.000000000",
 		"2006/01/02",
 		"2006/01/02 15:04:05",
+		"2006/01/02 15:04:05.000",
+		"2006/01/02 15:04:05.000000",
+		"2006/01/02 15:04:05.000000000",
 		"2006年01月02日",
 		"2006年01月02日 15:04:05",
+		"2006年01月02日 15:04:05.000",
+		"2006年01月02日 15:04:05.000000",
+		"2006年01月02日 15:04:05.000000000",
 		"2006年01月02日 15时04分05秒",
-		time.Kitchen,
-		time.Stamp,
-		time.StampMilli,
-		time.StampMicro,
-		time.StampNano,
 	}
 
 	for _, dateType := range fs {
